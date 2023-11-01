491d2883d24a170a11df81f7b9c69caf9886aaba
AsusIran <AsusIran@AMIRHAMED>
2023-11-01 14:14:54 +0330

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class GFG 
+{
+    public static int Fib(int n) 
+    {
+        if (n <= 1) 
+        {
+            return n; 
+        }
+        else 
+        {
+            return Fib(n - 1) + Fib(n - 2); 
+        } 
+    } 
+    public static void Main(string[] args) 
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write(Fib(n)); 
+    }
+}
+//امیرحسین جورابچی
+//4.عدد n ام فیبوناچی
